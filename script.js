daf52877fbf744034d9b68f319dbbe16bd535770
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-27 20:22:36 -0500

    finally got the navigation bar formatted correctly

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    console.log(vh);
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+    if (document.documentElement.scrollTop <= (vh/2)){
+        navbar.style.height = "50vh";
+    } else if (document.documentElement.scrollTop <= vh){
+        let newH = ((100 -(((vh/2 - document.documentElement.scrollTop)/vh)*-100))- 50);
+        if (newH > 8){
+            navbar.style.height = newH + "vh";
+        }else{
+            navbar.style.height = "8vh";
+        }
+    } else{
+        navbar.style.height = "8vh";
+    }
+}
